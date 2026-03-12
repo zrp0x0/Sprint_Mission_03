23a381cbf6966e46df41a3af192e5463870ecca0
zrp0x0 <zrp0x0@gmail.com>
2026-03-12 18:07:43 +0900

    first commit

---
+++ src/main/java/com/sprint/mission/discodeit/SprintMission03Application.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintMission03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintMission03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprint/mission/discodeit/SprintMission03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintMission03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
