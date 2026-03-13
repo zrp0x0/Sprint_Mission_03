003f7138dbe6e0530081bd743d7352f7510548b9
zrp0x0 <zrp0x0@gmail.com>
2026-03-13 18:19:36 +0900

    [UPDATE] 추가 기능 요구사항 새로운 도메인 추가하기 수정

---
+++ src/main/java/com/sprint/mission/discodeit/repository/BinaryContentRepository.java
@@ -0,0 +1,9 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.repository.base.Repository;
+
+import java.util.UUID;
+
+public interface BinaryContentRepository extends Repository<BinaryContent, UUID> {
+}
---
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -0,0 +1,9 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.repository.base.Repository;
+
+import java.util.UUID;
+
+public interface ReadStatusRepository extends Repository<ReadStatus, UUID> {
+}
