755417496b928e1380e3059c1eeff0a90e326843
zrp0x0 <zrp0x0@gmail.com>
2026-03-13 16:15:12 +0900

    [UPDATE] 시간 타입 변경하기 Long => Instant

--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.entity.base;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter // equals and hash도 해야될 듯? - 공부하고 하자
@@ -11,12 +12,12 @@ public abstract class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     protected UUID id;
-    protected Long createAt;
-    protected Long updateAt;
+    protected Instant createAt;
+    protected Instant updateAt;
 
     protected BaseEntity() {
         this.id = UUID.randomUUID();
-        this.createAt = System.currentTimeMillis(); // ms 단위
+        this.createAt = Instant.now(); // ms 단위
         this.updateAt = createAt;
     }
 
@@ -27,7 +28,7 @@ public abstract class BaseEntity implements Serializable {
     }
 
     protected void touch() {
-        this.updateAt = System.currentTimeMillis();
+        this.updateAt = Instant.now();
     }
 
     public abstract BaseEntity copy();
